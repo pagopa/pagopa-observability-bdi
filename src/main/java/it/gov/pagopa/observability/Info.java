ccfc57ed2246bd2724b672360a3ca2790f96d956
Andrea Ferracci <andrea.ferracci@gmail.com>
2025-02-18 00:04:22 +0100

    fix info function

--- src/main/java/it/gov/pagopa/observability/Info.java
+++ src/main/java/it/gov/pagopa/observability/Info.java
@@ -9,7 +9,6 @@ import  it.gov.pagopa.observability.models.AppInfo;
 import java.io.InputStream;
 import java.util.Optional;
 import java.util.Properties;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -29,27 +28,31 @@ public class Info {
 			authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
 			final ExecutionContext context) {
 
-		context.getLogger().log(Level.INFO, "Invoked health check HTTP trigger for pagopa-observability-bdi.");
+		Logger logger = context.getLogger();
+
+		logger.info("Info - invoked health check HTTP trigger for pagopa-observability-bdi");
 		return request.createResponseBuilder(HttpStatus.OK)
 				.header("Content-Type", "application/json")
-				.body(getInfo(context.getLogger(), "/META-INF/maven/it.gov.pagopa.reporting/reporting-batch/pom.properties"))
+				.body(getInfo(logger, "/META-INF/maven/it.gov.pagopa.observability/bdi-observability-functions/pom.properties"))
 				.build();
 	}
 
 	public synchronized AppInfo getInfo(Logger logger, String path) {
 		String version = null;
 		String name = null;
+		String env = System.getenv("ENVIRONMENT");
 		try {
 			Properties properties = new Properties();
 			InputStream inputStream = getClass().getResourceAsStream(path);
 			if (inputStream != null) {
 				properties.load(inputStream);
 				version = properties.getProperty("version", null);
-				name = properties.getProperty("artifactId", null);
+				name = properties.getProperty("artifactId", "bdi-observability-functions");
+				logger.info(String.format("Info - version [%s] name [%s] environment [%s]", version, name, env));
 			}
 		} catch (Exception e) {
-			logger.severe("Impossible to retrieve information from pom.properties file.");
+			logger.severe("Info - impossible to retrieve information from pom.properties file.");
 		}
-		return AppInfo.builder().version(version).environment("azure-fn").name(name).build();
+		return AppInfo.builder().version(version).environment(env).name(name).build();
 	}
 }
